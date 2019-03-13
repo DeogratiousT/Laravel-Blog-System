7f858d642225e86d94754becea1f199bf7661640
unknown <deogratioust@gmail.com>
2019-03-13 13:56:13 +0300

    Navbar updated

--- public/js/app.js
+++ public/js/app.js
@@ -48944,8 +48944,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\myblog\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\myblog\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\Laravel-Blog-System\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\Laravel-Blog-System\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,64 +1,59 @@
-<nav class="navbar navbar-expand-md navbar-dark bg-dark">
-    <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Teddius') }}
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/">HOME <span class="sr-only">(current)</span></a>
-                    </li>        
-                    <li class="nav-item">
-                        <a class="nav-link" href="/about">ABOUT</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/services">SERVICES</a>
-                    </li>
-                    <li class="nav-item">
-                            <a class="nav-link" href="/posts">BLOG</a>
-                    </li>
-                </ul>
+<nav class="fixed">
+    <div class="row">
+        <div class="col-md-4 col-sm-4">
+            <a class="navbar-brand">
+                <img src="storage/images/logo.png" alt="My Logo" style="height: 60px; margin: 25% 80%;">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        <div class="col-md-8 col-sm-8" id="navbarsExampleDefault">
+            <ul class="navul">
+                <li class="navli">
+                    <a href="/">HOME <span class="sr-only">(current)</span></a>
+                </li>        
+                <li class="navli">
+                    <a href="/about">ABOUT</a>
+                </li>
+                <li class="navli">
+                    <a href="/services">SERVICES</a>
+                </li>
+                <li class="navli">
+                        <a href="/posts">BLOG</a>
+                </li>
+            </ul>                
                 
-            </div>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
-
-            </ul>
-
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto">
+            <ul class="navulright">
                 <!-- Authentication Links -->
                 @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    <li class="navliright">
+                        <a href="{{ route('login') }}">{{ __('Login') }}</a>
                     </li>
                     @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        <li class="navliright">
+                            <a href="{{ route('register') }}">{{ __('Register') }}</a>
                         </li>
                     @endif
                 @else
-                    <li class="nav-item dropdown">
+                    <li class="navliright" style="float:left;">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
-                                                       
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="/dashboard" >Dashboard</a>
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                            onclick="event.preventDefault();
-                                            document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
+                                                    
+                        <ul class="navuldropdown">
+                            <li class="navuldropdownli"><a href="/dashboard" >Dashboard</a></li>
+                            <li class="navuldropdownli"><a href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                            </li>
 
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                 @csrf
                             </form>
-                        </div>
+                        </ul>
                     </li>
                 @endguest
             </ul>
